e946e466f33491ed2c20031b45166141a6117f75
Mike McNeil <mike@balderdash.co>
2014-04-25 17:31:04 -0500

    Setup for new http config file included by default, and normalized some formatting

--- templates/config/globals.js
+++ templates/config/globals.js
@@ -1,3 +1,10 @@
+/**
+ * Configure which global variables which will be exposed
+ * automatically by Sails.
+ * 
+ * For more information on configuration, check out:
+ * http://sailsjs.org/#documentation
+ */
 module.exports.globals = {
 	_: true,
 	async: true,
