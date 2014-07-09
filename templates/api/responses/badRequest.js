2272bc327957efbc0888142559b7c1df1b2a5f5d
Mike McNeil <mike@balderdash.co>
2014-07-09 09:31:52 -0500

    Update response templates for consistency.  See f12794d60fdf57545ee7d711395349f8a98d40d3

--- templates/api/responses/badRequest.js
+++ templates/api/responses/badRequest.js
@@ -3,90 +3,78 @@
  *
  * Usage:
  * return res.badRequest();
- * return res.badRequest(err);
- * return res.badRequest(err, view);
- * return res.badRequest(err, redirectTo);
+ * return res.badRequest(data);
+ * return res.badRequest(data, 'some/specific/badRequest/view');
  *
  * e.g.:
  * ```
  * return res.badRequest(
  *   'Please choose a valid `password` (6-12 characters)',
- *   '/trial/signup'
+ *   'trial/signup'
  * );
  * ```
  */
 
-module.exports = function badRequest(err, viewOrRedirect) {
+module.exports = function badRequest(data, options) {
 
-  // Get access to `req` & `res`
+  // Get access to `req`, `res`, & `sails`
   var req = this.req;
   var res = this.res;
-
-  // Serve JSON (with optional JSONP support)
-  function sendJSON (data) {
-    if (!data) {
-      return res.send();
-    }
-    else {
-      if (typeof data !== 'object' || data instanceof Error) {
-        data = {error: data};
-      }
-      if ( req.options.jsonp && !req.isSocket ) {
-        return res.jsonp(data);
-      }
-      else return res.json(data);
-    }
-  }
+  var sails = req._sails;
 
   // Set status code
   res.status(400);
 
   // Log error to console
-  this.req._sails.log.verbose('Sent 400 ("Bad Request") response');
-  if (err) {
-    this.req._sails.log.verbose(err);
+  if (data !== undefined) {
+    sails.log.verbose('Sending 400 ("Bad Request") response: \n',data);
+  }
+  else sails.log.verbose('Sending 400 ("Bad Request") response');
+
+  // Only include errors in response if application environment
+  // is not set to 'production'.  In production, we shouldn't
+  // send back any identifying information about errors.
+  if (sails.config.environment === 'production') {
+    data = undefined;
   }
 
   // If the user-agent wants JSON, always respond with JSON
   if (req.wantsJSON) {
-    return sendJSON(err);
+    return res.jsonx(data);
   }
 
-  // Make data more readable for view locals
-  var locals;
-  if (!err) { locals = {}; }
-  else if (typeof err !== 'object'){
-    locals = {error: err};
-  }
-  else {
-    var readabilify = function (value) {
-      if (sails.util.isArray(value)) {
-        return sails.util.map(value, readabilify);
-      }
-      else if (sails.util.isPlainObject(value)) {
-        return sails.util.inspect(value);
-      }
-      else return value;
-    };
-    locals = { error: readabilify(err) };
+  // If second argument is a string, we take that to mean it refers to a view.
+  // If it was omitted, use an empty object (`{}`)
+  options = (typeof options === 'string') ? { view: options } : options || {};
+
+  // If a view was provided in options, serve it.
+  // Otherwise try to guess an appropriate view, or if that doesn't
+  // work, just send JSON.
+  if (options.view) {
+    return res.view(options.view, { data: data });
   }
 
-  // Serve HTML view or redirect to specified URL
-  if (typeof viewOrRedirect === 'string') {
-    if (viewOrRedirect.match(/^(\/|http:\/\/|https:\/\/)/)) {
-      if (err && typeof req.flash === 'function') {
-        req.flash('error', err);
+  // If no second argument provided, try to serve the default view,
+  // but fall back to sending JSON(P) if any errors occur.
+  else return res.view('400', { data: data }, function (err, html) {
+
+    // If a view error occured, fall back to JSON(P).
+    if (err) {
+      //
+      // Additionally:
+      // • If the view was missing, ignore the error but provide a verbose log.
+      if (err.code === 'E_VIEW_FAILED') {
+        sails.log.verbose('res.badRequest() :: Could not locate view for error page (sending JSON instead).  Details: ',err);
       }
-      return res.redirect(viewOrRedirect);
+      // Otherwise, if this was a more serious error, log to the console with the details.
+      else {
+        sails.log.warn('res.badRequest() :: When attempting to render error page view, an error occured (sending JSON instead).  Details: ', err);
+      }
+      return res.jsonx(data);
     }
-    else return res.view(viewOrRedirect, locals, function viewReady(viewErr, html) {
-      if (viewErr) return sendJSON(err);
-      else return res.send(html);
-    });
-  }
-  else return res.view('400', locals, function viewReady(viewErr, html) {
-    if (viewErr) return sendJSON(err);
-    else return res.send(html);
+
+    return res.send(html);
   });
+
 };
 
