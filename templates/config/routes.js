d5fccd944ec1425a2e9447c8b6986c996ff0587a
jub3i <anon66@gmail.com>
2015-03-06 11:48:10 +0200

    Update routes.js
    spacing off by one

--- templates/config/routes.js
+++ templates/config/routes.js
@@ -40,7 +40,7 @@ module.exports.routes = {
   *                                                                          *
   * Custom routes here...                                                    *
   *                                                                          *
-  *  If a request to a URL doesn't match any of the custom routes above, it  *
+  * If a request to a URL doesn't match any of the custom routes above, it   *
   * is matched against Sails route blueprints. See `config/blueprints.js`    *
   * for configuration options and examples.                                  *
   *                                                                          *
