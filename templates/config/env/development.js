fa8646d4d01e126834976d23dae79b19811580c0
Sander Boom <sander@inflowmotion.nl>
2015-07-08 21:09:21 +0200

    Some extra space got removed

--- templates/config/env/development.js
+++ templates/config/env/development.js
@@ -5,7 +5,7 @@
  * such as API keys or remote database passwords.  If you're using
  * a version control solution for your Sails app, this file will
  * be committed to your repository unless you add it to your .gitignore
- * file.  If your repository will be publicly viewable, don't add
+ * file. If your repository will be publicly viewable, don't add
  * any private information to this file!
  *
  */
