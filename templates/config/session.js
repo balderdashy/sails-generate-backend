7fbf40abbf854d975b795632ea9309cc8a352c98
sgress454 <scottmgress@gmail.com>
2016-02-16 12:23:16 -0600

    Update session.js

--- templates/config/session.js
+++ templates/config/session.js
@@ -75,7 +75,7 @@ module.exports.session = {
   ***************************************************************************/
 
   // adapter: 'mongo',
-  // url: 'mongodb://localhost:27017/dbname',
+  // url: 'mongodb://user:password@localhost:27017/dbname', // user, password and port optional
 
   /***************************************************************************
   *                                                                          *
